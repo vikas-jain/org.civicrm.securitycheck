b1649d5c4c39c2ca6144cec15e07d48ef60e9d0e
Vikas Jain <vikas@vikas-System-Product-Name.(none)>
2014-02-24 20:02:47 +0530

    updated by nisha

--- securitycheck.php
+++ securitycheck.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'securitycheck.civix.php';
-
+require_once 'Security.php';
 /**
  * Implementation of hook_civicrm_config
  */
@@ -99,207 +99,21 @@ function getFilePathMarker() {
 function createDocUrl($topic) {
   return CRM_Utils_System::getWikiBaseURL() . $topic;
 }
-
 /**
- * Check if our logfile is directly accessible.
+ * 
+ *Displays Security Error Messages On Civicrm Administrator page
  *
- * Per CiviCRM default the logfile sits in a folder which is
- * web-accessible, and is protected by a default .htaccess
- * configuration. If server config causes the .htaccess not to
- * function as intended, there may be information disclosure.
- *
- * The debug log may be jam-packed with sensitive data, we don't
- * want that.
- *
- * Being able to be retrieved directly doesn't mean the logfile
- * is browseable or visible to search engines; it means it can be
- * requested directly.
- *
- * @return array of messages
- * @see CRM-14091
  */
-function checkLogFileIsNotAccessible() {
-  $messages = array();
-  $config = CRM_Core_Config::singleton();
-  $log = CRM_Core_Error::createDebugLogger();
-  $log_filename = $log->_filename;
-  $filePathMarker = getFilePathMarker();
-  // Hazard a guess at the URL of the logfile, based on common
-  // CiviCRM layouts.
-  if ($upload_url = explode($filePathMarker, $config->imageUploadURL)) {
-    $url[] = $upload_url[0];
-    if ($log_path = explode($filePathMarker, $log_filename)) {
-      $url[] = $log_path[1];
-      $log_url = implode($filePathMarker, $url);
-      $docs_url = createDocUrl('checkLogFileIsNotAccessible');
-      if ($log = @file_get_contents($log_url)) {
-        $msg = 'The <a href="%1">CiviCRM debug log</a> should not be downloadable.'
-          . '<br />' .
-          '<a href="%2">Read more about this warning</a>';
-        $messages[] = ts($msg, array(1 => $log_url, 2 => $docs_url));
-      }
-    }
-  }
-
-  return $messages;
-}
-
-/**
- * Check if our uploads directory has accessible files.
- *
- * We'll test a handful of files randomly. Hazard a guess at the URL
- * of the uploads dir, based on common CiviCRM layouts. Try and
- * request the files, and if any are successfully retrieved, warn.
- *
- * Being retrievable doesn't mean the files are browseable or visible
- * to search engines; it only means they can be requested directly.
- *
- * @return array of messages
- * @see CRM-14091
- *
- * @TODO: Test with WordPress, Joomla.
- */
-function checkUploadsAreNotAccessible() {
-  $messages = array();
-  $config = CRM_Core_Config::singleton();
-  $filePathMarker = getFilePathMarker();
-  
-  if ($upload_url = explode($filePathMarker, $config->imageUploadURL)) {
-    if ($files = glob($config->uploadDir . '/*')) {
-      for ($i = 0;$i < 3; $i++) {
-        $f = array_rand($files);
-        if ($file_path = explode($filePathMarker, $files[$f])) {
-          $url = implode($filePathMarker, array($upload_url[0], $file_path[1]));
-          if ($file = @file_get_contents($url)) {
-            $msg = 'Files in the upload directory should not be downloadable.'
-              . '<br />' .
-              '<a href="%1">Read more about this warning</a>';
-            $docs_url = createDocUrl('checkUploadsAreNotAccessible');
-            $messages[] = ts($msg, array(1 => $docs_url));
-            
-          }
-        }
-      }
-    }
-  }
-  
-  return $messages;
-}
-
-/**
- * Determine whether $url is a public, browsable listing for $dir
- *
- * @param string $dir local dir path
- * @param string $url public URL
- * @return bool
- */
-function isBrowsable($dir, $url) {
-  if (empty($dir) || empty($url) || !is_dir($dir)) {
-    return FALSE;
-  }
-  
-  $result = FALSE;
-  $file = 'delete-this-' . CRM_Utils_String::createRandom(10, CRM_Utils_String::ALPHANUMERIC);
-  
-  // this could be a new system with no uploads (yet) -- so we'll make a file
-  file_put_contents("$dir/$file", "delete me");
-  $content = @file_get_contents("$url");
-  if (stristr($content, $file)) {
-    $result = TRUE;
-  }
-  unlink("$dir/$file");
-  
-  return $result;
-}
-
-/**
- * Restrict remote users from browsing the given directory.
- *
- * @param $publicDir
- */
-function restrictBrowsing($publicDir) {
-  if (!is_dir($publicDir) || !is_writable($publicDir)) {
-    return;
-  }
-  
-  // base dir
-  $nobrowse = realpath($publicDir) . '/index.html';
-  if (!file_exists($nobrowse)) {
-    @file_put_contents($nobrowse, '');
-  }
-  
-  // child dirs
-  $dir = new RecursiveDirectoryIterator($publicDir);
-  foreach ($dir as $name => $object) {
-    if (is_dir($name) && $name != '..') {
-      $nobrowse = realpath($name) . '/index.html';
-      if (!file_exists($nobrowse)) {
-        @file_put_contents($nobrowse, '');
-      }
-    }
-  }
-}
-
-/**
- * Check if our uploads or ConfigAndLog directories have browseable
- * listings.
- *
- * Retrieve a listing of files from the local filesystem, and the
- * corresponding path via HTTP. Then check and see if the local
- * files are represented in the HTTP result; if so then warn. This
- * MAY trigger false positives (if you have files named 'a', 'e'
- * we'll probably match that).
- *
- * @return array of messages
- * @see CRM-14091
- *
- * @TODO: Test with WordPress, Joomla.
- */
-function checkDirectoriesAreNotBrowseable() {
-  $messages = array();
-  $config = CRM_Core_Config::singleton();
-  $publicDirs = array(
-    $config->imageUploadDir => $config->imageUploadURL,
-  );
-  
-  // Setup index.html files to prevent browsing
-  foreach ($publicDirs as $publicDir => $publicUrl) {
-    restrictBrowsing($publicDir);
-  }
-  
-  // Test that $publicDir is not browsable
-  foreach ($publicDirs as $publicDir => $publicUrl) {
-    if (isBrowsable($publicDir, $publicUrl)) {
-      $msg = 'Directory <a href="%1">%2</a> should not be browseable via the web.'
-        . '<br />' .
-        '<a href="%3">Read more about this warning</a>';
-      $docs_url = createDocUrl('checkDirectoriesAreNotBrowseable');
-      $messages[] = ts($msg, array(1 => $publicDir, 2 => $publicDir, 3 => $docs_url));
-      
-    }
-  }
-  return $messages;
-}
-
-function checkAll() {
-  $messages = array_merge(checkLogFileIsNotAccessible(),
-              checkUploadsAreNotAccessible(),
-              checkDirectoriesAreNotBrowseable()
-  );
-  
-  return $messages;
-}
-
 function securitycheck_civicrm_pageRun( &$page ){
-  if($page->getvar('_name') == "CRM_Admin_Page_Admin") {
-    if (CRM_Core_Permission::check('administer CiviCRM')) {
-      $session = CRM_Core_Session::singleton();
-      // Best attempt at re-securing folders
-      $config = CRM_Core_Config::singleton();
-      $config->cleanup(0, FALSE);
-      foreach (checkAll() as $message) {
-        CRM_Core_Session::setStatus($message, ts('Security Warning'));
-      }
+    if($page->getvar('_name') == "CRM_Admin_Page_Admin") {
+        if (CRM_Core_Permission::check('administer CiviCRM')) {
+            $check = CRM_Utils_Check_Security::singleton();
+            $messages = $check->checkAll();
+            $config = CRM_Core_Config::singleton();
+            $config->cleanup(0, FALSE);
+            foreach ($messages as $message) {
+                CRM_Core_Session::setStatus($message, ts('Security Warning'));
+            }
+        }
     }
-  }
 }
